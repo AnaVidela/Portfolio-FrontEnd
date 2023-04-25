c33b036c9f3b9744fbb34ff5654d39d4f884e595
Analía <analia.videla.84@gmail.com>
2023-04-25 20:37:27 -0300

    Validators en form-register

--- FrontEnd/src/app/components/form-register/form-register.component.ts
+++ FrontEnd/src/app/components/form-register/form-register.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.css']
 })
-export class FormRegisterComponent {
+export class FormRegisterComponent implements OnInit{
+  register: FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+    this.register=this.formBuilder.group({
+      name: ['', [Validators.required,Validators.minLength(1)]],
+      apellido: ['',[Validators.required,Validators.minLength(1)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
+
+  ngOnInit(): void {    
+  }
+  get Name(){
+    return this.register.get("name");
+  }
+
+  get Apellido() {
+    return this.register.get("apellido");
+  }
+
+  get Password(){
+    return this.register.get("password");
+  }
+  
+  get Mail() {
+    return this.register.get("email");
+  }
+  
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+  
+  get MailValid(){
+    return false;
+  }
+
+  onEnviar(event: Event) {
+    event.preventDefault;
+    if (this.register.valid){
+      alert("Todo correcto ¡Enviar formulario!");
+    }else {
+      this.register.markAllAsTouched();
+    }
+  }
 }
